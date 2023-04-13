50111c4038671714661ef50b1fc0e2264abf1c94
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-13 13:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const body = document.querySelector('body');
+  
+  // Clear the body content
+  body.innerHTML = '';
+  
+  // Create a new text node
+  const text = document.createTextNode('DOM load success');
+  
+  // Append the text node to the body
+  body.appendChild(text);
